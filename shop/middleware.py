d3aad940b6bb37a46b2548d219bdd2f8826914f5
aaxaa <xucj21@gmail.com>
2016-01-15 02:06:34 +0800

    dev

--- shop/middleware.py
+++ shop/middleware.py
@@ -5,6 +5,7 @@ from urllib import quote
 
 class WxMiddleware(object):
 	def process_request(self, request):
-		if 'wx_code' not in request.session:
+		print request.META['HTTP_REFERER']
+		if request.META['HTTP_REFERER'] != '' and 'wx_code' not in request.session:
 			return redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=a1b2c3#wechat_redirect'%(WECHAT_APPID, quote('http://shop.baremeii.com/wx_callback')))
 		
