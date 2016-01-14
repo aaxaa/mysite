ac727f4a2b457a72ce2abd9c94f44f87da25f8f1
aaxaa <xucj21@gmail.com>
2016-01-15 02:02:09 +0800

    dev

--- shop/views.py
+++ shop/views.py
@@ -544,7 +544,7 @@ def wx_verify(request):
     else:
         return HttpResponse('error')
 
-def wxpay_callback(request):
+def wx_callback(request):
     if 'wx_code' in request.session:
         r = requests.get('https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code'%(WECHAT_APPID, WECHAT_APPSECRET, request.GET.get('code')))
         if int(r.status_code) == 200:
