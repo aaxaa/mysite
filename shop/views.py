e1e3e7bbce3487ee9cf8f54394d6b405ab302b55
xjay <xuchengjie@banggood.cn>
2016-01-15 17:28:16 +0800

    dev

--- shop/views.py
+++ shop/views.py
@@ -7,7 +7,7 @@ from django.db import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 
 from shop.models import Notice, Product, Setting, Customer, Category, Shopcart, ShopcartProduct, Order, OrderProduct, CustomerRelation, CustomerConnect, Message
-from shop.utils import build_form_by_params, get_client_ip
+from shop.utils import generate_random_string, get_client_ip
 from main.settings import EMAY_SN, EMAY_KEY, EMAY_PWD, WECHAT_APPID, WECHAT_APPSECRET, WECHAT_TOKEN
 
 from decimal import *
@@ -572,11 +572,32 @@ def wx_callback(request):
                 customer_connect.save()
 
             request.session['wx_code'] = request.GET.get('code')
+            request.session['openid'] = customer_connect['openid']
             return redirect('/')
         else:
             return HttpResponse(u'获取个人信息时，网络出现问题！')
     else:
         return HttpResponse(u'网络出现故障，请返回重试！')
+
+def wxpay_test(request):
+    data = {
+        'appid':WECHAT_APPID,
+        'timestamp':str(int(time.time())),
+        'noncestr':generate_random_string()
+    }
+    # params = build_form_by_params({
+    #     'body': 'product test',
+    #     'out_trade_no' : '001',
+    #     'total_fee':0.01,
+    #     'spbill_create_ip':get_client_ip(request)
+    # })
+
+    # data['wx_pay_params'] = json.dumps(params)
+    wx = WechatBasic(token=WECHAT_TOKEN, appid=WECHAT_APPID, appsecret=WECHAT_APPSECRET)
+    data['signature'] = wx.generate_jsapi_signature(timestamp=data['timestamp'], noncestr=data['noncestr'], url="http://shop.baremeii.com/wxpay_test")
+
+    return render(request, 'wxpay_test.html', data)
+
 
 def wxpay_notify(request):
     return render(request, 'wxpay_notify.html')
