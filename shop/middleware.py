f604c197de0d45de721083017411cd15e0a432fa
xjay <xucj@mama.cn>
2016-01-16 17:27:42 +0800

    dev

--- shop/middleware.py
+++ shop/middleware.py
@@ -10,9 +10,9 @@ class WxMiddleware(object):
 		if (not request.path.startswith('/admin/')) and 'code' not in request.GET and 'state' not in request.GET:
 			if 'openid' in request.session and request.session['openid']:
 
-				#del request.session['openid']
+				
 				customer_connect = CustomerConnect.objects.get(openid=request.session['openid'])
-
+				del request.session['openid']
 				if not customer_connect.customer:
 					return redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=a1b2c3#wechat_redirect'%(WECHAT_APPID, quote('http://shop.baremeii.com/wx_callback')))
 			else:
