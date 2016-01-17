958a2fe2a860219b1bcb86fb7f15ea2e2cd1611b
xjay <xucj@mama.cn>
2016-01-18 04:40:24 +0800

    dev

--- shop/middleware.py
+++ shop/middleware.py
@@ -7,6 +7,6 @@ from shop.models import CustomerConnect
 
 class WxMiddleware(object):
 	def process_request(self, request):
-		if (not request.path.startswith('/admin/')) and 'code' not in request.GET and 'state' not in request.GET and 'openid' not in request.session:
+		if (not request.path.startswith('/admin/')) and (not request.path.startswith('/wxpay_notify/')) and 'code' not in request.GET and 'state' not in request.GET and 'openid' not in request.session:
 			return redirect('https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=snsapi_userinfo&state=%s#wechat_redirect'%(WECHAT_APPID, quote('http://shop.baremeii.com/wx_callback/'),request.path))
 		
