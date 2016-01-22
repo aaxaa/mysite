0f4038720a2287d538e18cf22abfdffc753872ae
xjay <xucj@mama.cn>
2016-01-22 15:39:52 +0800

    dev

--- shop/views.py
+++ shop/views.py
@@ -664,16 +664,15 @@ def wxpay_notify(request):
 
                 order.status = 3
                 order.save()
-                print order.status
 
-                shopcart = Shopcart.objects.get(customer=customer_connect.customer)
-                shopcart.delete()
+                #shopcart = Shopcart.objects.get(customer=customer_connect.customer)
+                #shopcart.delete()
 
-            # pids = [int(product.id) for product in order.products_in.all()]
-            
-            # sps = ShopcartProduct.objects.filter(shopcart__customer=order.customer, product__id__in=pids)
-            # print sps
-            # sps.delete()
+                pids = [int(product.id) for product in order.products_in.all()]
+                
+                sps = ShopcartProduct.objects.filter(shopcart__customer=order.customer, product__id__in=pids)
+                print sps
+                sps.delete()
 
                 return HttpResponse(dict_to_xml({'return_code':'SUCCESS','return_msg':'OK'}))
     return HttpResponse(dict_to_xml({'return_code':'FAILED','return_msg':'ERROR'}))
