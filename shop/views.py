0f4501396885f931dee33a6af688214cd7a678dd
xjay <xucj@mama.cn>
2016-01-27 18:46:52 +0800

    dev

--- shop/views.py
+++ shop/views.py
@@ -52,7 +52,7 @@ def notice(request, id=0):
 
 def order(request):
     if 'customer' in request.session and request.session['customer']:
-        tabstatus = request.GET.get('tabstatus', 'all')
+        tabstatus = 'all'
         order_data = []
         try:
             customer = Customer.objects.get(id=request.session['customer'].get('id'))
