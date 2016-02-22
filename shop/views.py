5420bab138748cf8929a0bb8377dac5c922d46dc
xjay <xucj@mama.cn>
2016-02-22 23:31:20 +0800

    dev

--- shop/views.py
+++ shop/views.py
@@ -542,11 +542,19 @@ def shopcart_order_checkout(request):
             data['order_id'] = order_id
             data['order_txt'] = order.order_txt
             data['products'] = ''
+            data['total_point'] = 0
+            
             total_price = 0
             for product in order.products_in.all():
-                data['products'] += u"%s * %s = ￥%s<br/>" % (product.product.name, product.count, product.price)
+                if product.product.payment_type == 1:
+                    data['total_point'] += product.product.payment_point * product.count
+                    data['products'] += u"%s * %s = <del>￥%s</del><br/>" % (product.product.name, product.count, product.price)
+                elif product.product.payment_type == 3:
+                    data['total_point'] += product.product.payment_point * product.count
+                    data['products'] += u"%s * %s = ￥%s<br/>" % (product.product.name, product.count, product.price)
+                else:
+                    data['products'] += u"%s * %s = ￥%s<br/>" % (product.product.name, product.count, product.price)
                 total_price += float(product.price)
-
 
             data['products_all'] = order.products.all()
 
